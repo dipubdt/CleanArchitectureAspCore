1d7be41eca59252c01cbccb1021bdd3f5b359d78
dipon <dipon@gmail.com>
2023-07-31 11:32:04 +0600

    HandleQuery-and-HandleCommand-Add

--- src/Libraries/Infrastructur/HomeWork.Core/Command/UpdateStudent.cs
+++ src/Libraries/Infrastructur/HomeWork.Core/Command/UpdateStudent.cs
@@ -5,15 +5,16 @@ using HomeWork.Models;
 using HomeWork.Repositories.Base;
 using HomeWork.Repositories.Interfaces;
 using HomeWork.Service.Models;
+using HomeWork.Shared.Model;
 using MediatR;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace HomeWork.Core.Command;
 
-public  class UpdateLaptop
-{
-	public record UpdateLaptopCommand(int id, VmLaptop laptop) : IRequest<VmLaptop>;
 
-	public class UpdateLaptopCommandHandler : IRequestHandler<UpdateLaptopCommand, VmLaptop>
+	public record UpdateLaptopCommand(int id, VmLaptop laptop) : IRequest<CommandResult<VmLaptop>> { }
+
+	public class UpdateLaptopCommandHandler : IRequestHandler<UpdateLaptopCommand, CommandResult<VmLaptop>>
 	{
 		private readonly ILaptopRepository _laptopRepository;
 		private readonly IMapper _mapper;
@@ -25,18 +26,25 @@ public  class UpdateLaptop
 
 		}
 
-		public async Task<VmLaptop> Handle(UpdateLaptopCommand request, CancellationToken cancellationToken)
-		{
+		public async Task <CommandResult<VmLaptop>> Handle(UpdateLaptopCommand request, CancellationToken cancellationToken)
+	{
 			var datas = _mapper.Map<Laptop>(request.laptop);
+		   var returnData = await _laptopRepository.Update(request.id, datas);
 
-			return await _laptopRepository.Update(request.id, datas);
+		if (returnData is not null and { Id: var id } && id > 0)
+		{
+			return new CommandResult<VmLaptop>(returnData, CommandResultTypeEnum.Success);
 		}
+		return new CommandResult<VmLaptop>(null, CommandResultTypeEnum.UnprocessableEntity);
+
+
 	}
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
