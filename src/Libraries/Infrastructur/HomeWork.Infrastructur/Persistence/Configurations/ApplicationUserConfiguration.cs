add3ed363dce765ada44be16964d6e1bfa15e9df
tamjid <tamjidakash100@gmail.com>
2023-08-23 15:27:15 +0600

    JWT Implemented

---
+++ src/Libraries/Infrastructur/HomeWork.Infrastructur/Persistence/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using HomeWork.Models.IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HomeWork.Infrastructure.Persistence.Configurations;
+
+public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        var hasher = new PasswordHasher<ApplicationUser>();
+
+        builder.HasData(new ApplicationUser
+        {
+            Id = "b74ddd14-6340-4840-95c2-db12554843e5",
+            UserName = "admin",
+            NormalizedUserName="ADMIN",
+            Email = "admin@gmail.com",
+            NormalizedEmail="ADMIN@GMAIL.COM",
+            PasswordHash=hasher.HashPassword(null, "P@ssword1"),
+            LockoutEnabled = false,
+            PhoneNumber = "1234567890",
+            EmailConfirmed = true,
+        });
+    }
+}
---
+++ src/Libraries/Infrastructur/TaskManagement.Service.Models/AuthModel/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeWork.Service.Models.AuthModel;
+
+public class LoginModel
+{
+    [Required]
+    [DataType(DataType.EmailAddress)]
+    [DefaultValue("admin@gmail.com")]
+    public string? Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [DefaultValue("P@ssword1")]
+    public string? Password { get; set; }
+
+    public bool RememberMe { get; set; }
+}
